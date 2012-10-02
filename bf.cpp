63164c996c13d8d69d8f50e7092ae1365892bd45
Gianni Rossi <gianni.rossi@transtelco.com.br>
2012-10-02 10:42:20 -0300

    Removed boost/regex.

--- bf.cpp
+++ bf.cpp
@@ -1,7 +1,6 @@
 #include "bf.h"
 
 #include <fstream>
-#include <boost/regex.hpp>
 
 #include <cerrno>
 #include <cstring>
@@ -34,37 +33,6 @@ int getNumCores()
 			{
 				buff[r] = 0;
 				ret = atoi(buff);
-			}
-		}
-	}
-
-	if (ret == 0)
-	{
-		// possible alternative:  "^processor.: [0-9]+$"
-		boost::regex processor("(?<=processor\\s:\\s).*");
-		std::ifstream cpuinfo("/proc/cpuinfo");
-		while(cpuinfo.good())
-		{
-			char buffer[1024];
-			cpuinfo.getline(buffer, sizeof(buffer));
-
-			try
-			{
-				std::string str = buffer;
-				auto it = str.begin(), end = str.end();
-				boost::match_results<std::string::iterator> what;
-
-				if (boost::regex_search(it, end, what, processor, boost::regex_constants::match_default | boost::regex_constants::match_stop))
-				{
-					for(int i = 0, size = what.size(); i != size; i++)
-						if (what[i].matched)
-						{
-							ret = std::max(atoi(std::string(what[i].first, what[i].second).c_str()) + 1, ret);
-						}
-				}
-			}
-			catch(...)
-			{
 			}
 		}
 	}
