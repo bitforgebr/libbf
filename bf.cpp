9ae8ccda012ebeebab23194fb336d4fbf49258ac
Gianni Rossi <gianni.rossi@transtelco.com.br>
2012-10-23 10:04:26 -0200

    Reformated code and added Errno implementation for ctors.

--- bf.cpp
+++ bf.cpp
@@ -9,112 +9,124 @@
 namespace bitforge
 {
 
+ErrnoException::ErrnoException(int error_no):
+    ExceptionWithMessage(strerror(error_no)),
+    m_errno(error_no)
+{
+}
+
+ErrnoException::ErrnoException(std::string err_msg, int error_no):
+    ExceptionWithMessage(err_msg),
+    m_errno(error_no)
+{
+}
+
 int getNumCores()
 {
-	int ret = 0;
+    int ret = 0;
 
-	ProcStreams getconf;
-	const char* const args[] = {"getconf", "_NPROCESSORS_ONLN"};
-	if (runAttachedProcess(&getconf, args))
-	{
-		struct timeval timeOut = {0, 500};
-		int maxFD = std::max(getconf.stdOut, getconf.stdErr);
-		fd_set fd;
-		FD_SET(getconf.stdOut, &fd);
-		FD_SET(getconf.stdErr, &fd);
+    ProcStreams getconf;
+    const char* const args[] = {"getconf", "_NPROCESSORS_ONLN"};
+    if (runAttachedProcess(&getconf, args))
+    {
+        struct timeval timeOut = {0, 500};
+        int maxFD = std::max(getconf.stdOut, getconf.stdErr);
+        fd_set fd;
+        FD_SET(getconf.stdOut, &fd);
+        FD_SET(getconf.stdErr, &fd);
 
-		int rc = select(maxFD + 1, &fd, nullptr, nullptr, &timeOut);
-		if(rc > 0 && FD_ISSET(getconf.stdOut, &fd))
-		{
-			std::size_t buffSize = 256;
-			char buff[buffSize];
-			auto r = read(getconf.stdOut, buff, buffSize);
-			if (r > 0)
-			{
-				buff[r] = 0;
-				ret = atoi(buff);
-			}
-		}
-	}
+        int rc = select(maxFD + 1, &fd, nullptr, nullptr, &timeOut);
+        if(rc > 0 && FD_ISSET(getconf.stdOut, &fd))
+        {
+            std::size_t buffSize = 256;
+            char buff[buffSize];
+            auto r = read(getconf.stdOut, buff, buffSize);
+            if (r > 0)
+            {
+                buff[r] = 0;
+                ret = atoi(buff);
+            }
+        }
+    }
 
-	return ret;
+    return ret;
 }
 
 bool runAttachedProcess(ProcStreams *streams, const char* const args[], const char* const env[])
 {
-	int pipeIn[2], pipeOut[2], pipeErr[2]; /* pipe */
+    int pipeIn[2], pipeOut[2], pipeErr[2]; /* pipe */
 
-	pipe(pipeIn);
-	pipe(pipeOut);
-	pipe(pipeErr);
+    pipe(pipeIn);
+    pipe(pipeOut);
+    pipe(pipeErr);
 
-	switch(fork())
-	{
-		case -1: // Error
-		{
-			std::cerr << "Forking error: " << strerror(errno) << std::endl;
-			return false;
-		}
+    switch(fork())
+    {
+        case -1: // Error
+        {
+            std::cerr << "Forking error: " << strerror(errno) << std::endl;
+            return false;
+        }
 
-		case 0: // child
-		{
-			/* stdin/stdout/stderr are replaced by pipes */
-			dup2 (pipeOut[0], fileno(stdin));
-			dup2 (pipeIn[1], fileno(stdout));
-			dup2 (pipeErr[1], fileno(stderr));
+        case 0: // child
+        {
+            /* stdin/stdout/stderr are replaced by pipes */
+            dup2 (pipeOut[0], fileno(stdin));
+            dup2 (pipeIn[1], fileno(stdout));
+            dup2 (pipeErr[1], fileno(stderr));
 
-			/* close part of pipe unneeded */
-			close (pipeOut[1]);
-			close (pipeIn[0]);
-			close (pipeErr[0]);
+            /* close part of pipe unneeded */
+            close (pipeOut[1]);
+            close (pipeIn[0]);
+            close (pipeErr[0]);
 
-			execvpe(args[0], const_cast<char* const*>(args), const_cast<char* const*>(env));
+            execvpe(args[0], const_cast<char* const*>(args), const_cast<char* const*>(env));
 
-			// execvpe should never return; if it does, its an error
-			std::cerr << "Forking error: " << strerror(errno) << std::endl;
-			return false;
-		}
+            // execvpe should never return; if it does, its an error
+            std::cerr << "Forking error: " << strerror(errno) << std::endl;
+            return false;
+        }
 
-		default: // parent
-		{
-			/* close part of pipe unneeded */
-			close(pipeOut[0]);
-			close(pipeIn[1]);
-			close(pipeErr[1]);
+        default: // parent
+        {
+            /* close part of pipe unneeded */
+            close(pipeOut[0]);
+            close(pipeIn[1]);
+            close(pipeErr[1]);
 
-			streams->stdIn = pipeOut[1];
-			streams->stdOut = pipeIn[0];
-			streams->stdErr = pipeErr[0];
+            streams->stdIn = pipeOut[1];
+            streams->stdOut = pipeIn[0];
+            streams->stdErr = pipeErr[0];
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
 
 /* Fast hash function */
 uint32_t fletcher32( uint16_t *data, ::std::size_t len )
 {
-	uint32_t sum1 = 0xffff, sum2 = 0xffff;
+    uint32_t sum1 = 0xffff, sum2 = 0xffff;
 
-	while (len)
-	{
-		unsigned tlen = len > 360 ? 360 : len;
-		len -= tlen;
-		do
-		{
-			sum1 += *data++;
-			sum2 += sum1;
-		}
-		while (--tlen);
+    while (len)
+    {
+        unsigned tlen = len > 360 ? 360 : len;
+        len -= tlen;
+        do
+        {
+            sum1 += *data++;
+            sum2 += sum1;
+        }
+        while (--tlen);
 
-		sum1 = (sum1 & 0xffff) + (sum1 >> 16);
-		sum2 = (sum2 & 0xffff) + (sum2 >> 16);
-	}
+        sum1 = (sum1 & 0xffff) + (sum1 >> 16);
+        sum2 = (sum2 & 0xffff) + (sum2 >> 16);
+    }
 
-	/* Second reduction step to reduce sums to 16 bits */
-	sum1 = (sum1 & 0xffff) + (sum1 >> 16);
-	sum2 = (sum2 & 0xffff) + (sum2 >> 16);
-	return sum2 << 16 | sum1;
+    /* Second reduction step to reduce sums to 16 bits */
+    sum1 = (sum1 & 0xffff) + (sum1 >> 16);
+    sum2 = (sum2 & 0xffff) + (sum2 >> 16);
+    return sum2 << 16 | sum1;
 }
 
 } // bitforge
--- bf.h
+++ bf.h
@@ -1,12 +1,12 @@
 /*
- * bf.h
- *
- *  Created on: Sep 03, 2012
- *      Author: gianni
- *
- * BitForge http://www.bitforge.com.br
- * Copyright (c) 2012 All Right Reserved,
- */
+* bf.h
+*
+*  Created on: Sep 03, 2012
+*      Author: gianni
+*
+* BitForge http://www.bitforge.com.br
+* Copyright (c) 2012 All Right Reserved,
+*/
 
 #ifndef __INCLUDE_LIBBF_BF_H_
 #define __INCLUDE_LIBBF_BF_H_
@@ -26,315 +26,313 @@ namespace bitforge
 class ExceptionWithMessage: public std::exception
 {
 public:
-	ExceptionWithMessage(std::string what) throw() : m_what(what) {};
-	virtual ~ExceptionWithMessage() throw() {};
+    ExceptionWithMessage(std::string what) throw() : m_what(what) {};
+    virtual ~ExceptionWithMessage() throw() {};
 
 private:
-	RawString m_what;
+    RawString m_what;
 
 public:
-	virtual const char* what() const throw()
-	{
-		return m_what.data();
-	};
+    virtual const char* what() const throw()
+    {
+        return m_what.data();
+    };
 };
 
-class ErrnoException
+class ErrnoException: public ExceptionWithMessage
 {
 private:
-	int			m_errno;
-	std::string	m_errmsg;
+    int m_errno;
 
 public:
-	ErrnoException(int error_no);
-	ErrnoException(std::string err_msg, int error_no);
+    ErrnoException(int error_no);
+    ErrnoException(std::string err_msg, int error_no);
 
-	int error_number() const {return m_errno;}
-	const std::string& error_message() const {return m_errmsg;}
+    int error_number() const { return m_errno; }
 };
 
 enum DateFormat
 {
-	dfSQL,
-	dfCompact
+    dfSQL,
+    dfCompact
 };
 
 inline std::string getDate(DateFormat dateFormat = dfSQL)
 {
-	char dateStr[32];
+    char dateStr[32];
 
-	time_t t;
-	struct tm tm;
+    time_t t;
+    struct tm tm;
 
-	::std::time(&t);
-	gmtime_r(&t, &tm);
+    ::std::time(&t);
+    gmtime_r(&t, &tm);
 
-	const char* format;
+    const char* format;
 
-	switch(dateFormat)
-	{
-		case dfSQL:
-			format = "'%.4d-%.2d-%.2d %.2d:%.2d:%.2d'";
-			break;
+    switch(dateFormat)
+    {
+        case dfSQL:
+            format = "'%.4d-%.2d-%.2d %.2d:%.2d:%.2d'";
+            break;
 
-		case dfCompact:
-		default:
-			format = "%.4d%.2d%.2d%.2d%.2d%.2d";
-			break;
-	}
+        case dfCompact:
+        default:
+            format = "%.4d%.2d%.2d%.2d%.2d%.2d";
+            break;
+    }
 
-	::std::snprintf(dateStr, sizeof(dateStr), format, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    ::std::snprintf(dateStr, sizeof(dateStr), format, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-	return dateStr;
+    return dateStr;
 }
 
 class ProcTimer
 {
 private:
-	struct timespec m_time;
+    struct timespec m_time;
 
-	ProcTimer(struct timespec ts) : m_time(ts) {}
+    ProcTimer(struct timespec ts) : m_time(ts) {}
 
 public:
-	ProcTimer()
-	{
-		clock_gettime(CLOCK_REALTIME, &m_time);
-	}
+    ProcTimer()
+    {
+        clock_gettime(CLOCK_REALTIME, &m_time);
+    }
 
-	ProcTimer operator- (const ProcTimer& other) const
-	{
-		struct timespec ts = other.m_time;
+    ProcTimer operator- (const ProcTimer& other) const
+    {
+        struct timespec ts = other.m_time;
 
-		if(m_time.tv_nsec > ts.tv_nsec)
-		{
-			ts.tv_nsec = m_time.tv_nsec - ts.tv_nsec;
-		}
-		else
-		{
-			auto d = ts.tv_nsec - m_time.tv_nsec;
-			ts.tv_sec--;
-			ts.tv_nsec = pow10(9) - d;
-		}
+        if(m_time.tv_nsec > ts.tv_nsec)
+        {
+            ts.tv_nsec = m_time.tv_nsec - ts.tv_nsec;
+        }
+        else
+        {
+            auto d = ts.tv_nsec - m_time.tv_nsec;
+            ts.tv_sec--;
+            ts.tv_nsec = pow10(9) - d;
+        }
 
-		ts.tv_sec = m_time.tv_sec - ts.tv_sec;
+        ts.tv_sec = m_time.tv_sec - ts.tv_sec;
 
-		return ProcTimer(ts);
-	}
+        return ProcTimer(ts);
+    }
 
-	double seconds() const
-	{
-		return static_cast<double>(m_time.tv_sec) + (static_cast<double>(m_time.tv_nsec) / pow10(9));
-	}
+    double seconds() const
+    {
+        return static_cast<double>(m_time.tv_sec) + (static_cast<double>(m_time.tv_nsec) / pow10(9));
+    }
 
-	double operator/ (double value) const
-	{
-		return value / seconds();
-	}
+    double operator/ (double value) const
+    {
+        return value / seconds();
+    }
 
-	bool operator> (int value) const
-	{
-		return seconds() > value;
-	}
+    bool operator> (int value) const
+    {
+        return seconds() > value;
+    }
 };
 
 inline std::ostream& operator<< (std::ostream& out, const ProcTimer& timer)
 {
-	out << round(timer.seconds() * 100) / 100;
-	return out;
+    out << round(timer.seconds() * 100) / 100;
+    return out;
 }
 
 template<typename T>
 void zero_init(T& t)
 {
-	memset(&t, 0, sizeof(T));
+    memset(&t, 0, sizeof(T));
 }
 
 template<typename T>
 bool is_zero(const T& t)
 {
-	const char* p = reinterpret_cast<const char*>(&t);
-	const char* end = p + (sizeof(T) % sizeof(int));
+    const char* p = reinterpret_cast<const char*>(&t);
+    const char* end = p + (sizeof(T) % sizeof(int));
 
-	while(p != end)
-	{
-		if(*p) return false;
+    while(p != end)
+    {
+        if(*p) return false;
 
-		p++;
-	}
+        p++;
+    }
 
-	const int* i = reinterpret_cast<const int*>(p);
+    const int* i = reinterpret_cast<const int*>(p);
 
-	const int* iend = i + (sizeof(T) - (sizeof(T) % sizeof(int)));
+    const int* iend = i + (sizeof(T) - (sizeof(T) % sizeof(int)));
 
-	while(i != iend)
-	{
-		if(*i) return false;
+    while(i != iend)
+    {
+        if(*i) return false;
 
-		i++;
-	}
+        i++;
+    }
 
-	return true;
+    return true;
 }
 
 // TODO - Check for a better solution
 // This is used instead of snprintf or lexical_cast due to their lack of availability
 inline char* uinttostr(char* buffer, const int bufferSize, unsigned int val)
 {
-	char* c = buffer + (bufferSize - 1);
-	*c = 0;
+    char* c = buffer + (bufferSize - 1);
+    *c = 0;
 
-	do
-	{
-		*--c = '0' + (val % 10);
-		val /= 10;
-	}
-	while(val);
+    do
+    {
+        *--c = '0' + (val % 10);
+        val /= 10;
+    }
+    while(val);
 
-	return c;
+    return c;
 }
 
 inline char* inttostr(char* buffer, const int bufferSize, int val)
 {
-	const bool neg = val < 0;
-	char* result =  const_cast<char*>(uinttostr(buffer, bufferSize, val));
+    const bool neg = val < 0;
+    char* result =  const_cast<char*>(uinttostr(buffer, bufferSize, val));
 
-	if(neg)
-		*--result = '-';
+    if(neg)
+        *--result = '-';
 
-	return result;
+    return result;
 }
 
 inline char* uinttostr(char* buffer, const int bufferSize, unsigned int val, int base)
 {
-	char* c = buffer + (bufferSize - 1);
-	*c = 0;
+    char* c = buffer + (bufferSize - 1);
+    *c = 0;
 
-	do
-	{
-		const unsigned int digit = (val % base);
+    do
+    {
+        const unsigned int digit = (val % base);
 
-		if(digit <= 9)
-			*--c = '0' + digit;
-		else
-			*--c = 'A' + digit - 10;
+        if(digit <= 9)
+            *--c = '0' + digit;
+        else
+            *--c = 'A' + digit - 10;
 
-		val /= base;
-	}
-	while(val);
+        val /= base;
+    }
+    while(val);
 
-	return c;
+    return c;
 }
 
 
 inline char* uinttohex(char* buffer, const int bufferSize, unsigned int val)
 {
-	return uinttostr(buffer, bufferSize, val, 16);
+    return uinttostr(buffer, bufferSize, val, 16);
 }
 
 inline char* inttohex(char* buffer, const int bufferSize, int val)
 {
-	return uinttostr(buffer, bufferSize, val, 16);
+    return uinttostr(buffer, bufferSize, val, 16);
 }
 
 inline char* uint64tostr(char* buffer, const int bufferSize, uint64_t val)
 {
-	char* c = buffer + (bufferSize - 1);
-	*c = 0;
+    char* c = buffer + (bufferSize - 1);
+    *c = 0;
 
-	do
-	{
-		*--c = '0' + (val % 10);
-		val /= 10;
-	}
-	while(val);
+    do
+    {
+        *--c = '0' + (val % 10);
+        val /= 10;
+    }
+    while(val);
 
-	return c;
+    return c;
 }
 
 inline char* int64tostr(char* buffer, const int bufferSize, int64_t val)
 {
-	const bool neg = val < 0;
-	char* result =  const_cast<char*>(uint64tostr(buffer, bufferSize, val));
+    const bool neg = val < 0;
+    char* result =  const_cast<char*>(uint64tostr(buffer, bufferSize, val));
 
-	if(neg)
-		*--result = '-';
+    if(neg)
+        *--result = '-';
 
-	return result;
+    return result;
 }
 
 //TODO Fixme
 inline unsigned char hextoint(const char* v)
 {
-	if (strlen(v) < 2)
-		return 0;
+    if (strlen(v) < 2)
+        return 0;
 
-	unsigned char res;
+    unsigned char res;
 
-	if (v[0] >= '0' && v[0] <= '9')
-		res = (v[0] - '0') << 4;
-	else if (v[0] >= 'a' && v[0] <= 'f')
-		res = ((v[0] - 'f') + 15) << 4;
-	else if (v[0] >= 'A' && v[0] <= 'F')
-		res = ((v[0] - 'F') + 15) << 4;
+    if (v[0] >= '0' && v[0] <= '9')
+        res = (v[0] - '0') << 4;
+    else if (v[0] >= 'a' && v[0] <= 'f')
+        res = ((v[0] - 'f') + 15) << 4;
+    else if (v[0] >= 'A' && v[0] <= 'F')
+        res = ((v[0] - 'F') + 15) << 4;
 
-	if (v[1] >= '0' && v[1] <= '9')
-		res += v[1] - '0';
-	else if (v[1] >= 'a' && v[1] <= 'f')
-		res += (v[1] - 'f') + 15;
-	else if (v[1] >= 'A' && v[1] <= 'F')
-		res += (v[1] - 'F') + 15;
+    if (v[1] >= '0' && v[1] <= '9')
+        res += v[1] - '0';
+    else if (v[1] >= 'a' && v[1] <= 'f')
+        res += (v[1] - 'f') + 15;
+    else if (v[1] >= 'A' && v[1] <= 'F')
+        res += (v[1] - 'F') + 15;
 
-	return res;
+    return res;
 }
 
 //TODO Fixme
 inline std::string intohex(unsigned char v)
 {
-	char result[] = "00";
-	char* c = &result[2];
+    char result[] = "00";
+    char* c = &result[2];
 
-	do
-	{
-		const unsigned char digit = (v % 16);
+    do
+    {
+        const unsigned char digit = (v % 16);
 
-		if(digit <= 9)
-			*--c = '0' + digit;
-		else
-			*--c = 'A' + digit - 10;
+        if(digit <= 9)
+            *--c = '0' + digit;
+        else
+            *--c = 'A' + digit - 10;
 
-		v /= 16;
-	}
-	while(v);
+        v /= 16;
+    }
+    while(v);
 
-	return result;
+    return result;
 }
 
 constexpr unsigned int strKey(const char* str)
 {
-	return ((!str) || (!str[0])) ? 0 :
-	       (!str[1]) ? (str[0] << 24) :
-	       (!str[2]) ? (str[0] << 24) + (str[1] << 16) :
-	       (!str[3]) ? (str[0] << 24) + (str[1] << 16) + (str[2] << 8) :
-	       (str[0] << 24) + (str[1] << 16) + (str[2] << 8) + (str[3]);
+    return ((!str) || (!str[0])) ? 0 :
+        (!str[1]) ? (str[0] << 24) :
+        (!str[2]) ? (str[0] << 24) + (str[1] << 16) :
+        (!str[3]) ? (str[0] << 24) + (str[1] << 16) + (str[2] << 8) :
+        (str[0] << 24) + (str[1] << 16) + (str[2] << 8) + (str[3]);
 }
 
 inline unsigned int strKey(const std::string& str)
 {
-	return strKey(str.c_str());
+    return strKey(str.c_str());
 }
 
 constexpr unsigned int striKey(const char* str)
 {
-	return ((!str) || (!str[0])) ? 0 :
-	       (!str[1]) ? ((str[0] | 0x20) << 24) :
-	       (!str[2]) ? ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) :
-	       (!str[3]) ? ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) + ((str[2] | 0x20) << 8) :
-	       ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) + ((str[2] | 0x20) << 8) + (str[3] | 0x20);
+    return ((!str) || (!str[0])) ? 0 :
+        (!str[1]) ? ((str[0] | 0x20) << 24) :
+        (!str[2]) ? ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) :
+        (!str[3]) ? ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) + ((str[2] | 0x20) << 8) :
+        ((str[0] | 0x20) << 24) + ((str[1] | 0x20) << 16) + ((str[2] | 0x20) << 8) + (str[3] | 0x20);
 }
 
 inline unsigned int striKey(const std::string& str)
 {
-	return striKey(str.c_str());
+    return striKey(str.c_str());
 }
 
 // Get the number of cores reported by the OS.
@@ -343,27 +341,27 @@ int getNumCores();
 
 struct ProcStreams
 {
-	int stdIn;
-	int stdOut;
-	int stdErr;
+    int stdIn;
+    int stdOut;
+    int stdErr;
 
-	ProcStreams(): stdIn(0), stdOut(1), stdErr(3) {}
-	
-	~ProcStreams()
-	{
-		close(stdIn);
-		close(stdOut);
-		close(stdErr);
-	}
+    ProcStreams(): stdIn(0), stdOut(1), stdErr(3) {}
+
+    ~ProcStreams()
+    {
+        close(stdIn);
+        close(stdOut);
+        close(stdErr);
+    }
 };
 
 /**
- *	Start a process, but connect the process' stdin, stdout and strerr to pipes which are then
- *  returned to the caller in the @streams param.
- *  @param streams out structre to recieve the pipes for the process's streams.
- *  @param args process name and arguments.  args[0] should be the exec name (i.e. /bin/ls)
- *  @param env enviroment for the new process.  The default is a copy of the current process' enviroment.
- */
+*	Start a process, but connect the process' stdin, stdout and strerr to pipes which are then
+*  returned to the caller in the @streams param.
+*  @param streams out structre to recieve the pipes for the process's streams.
+*  @param args process name and arguments.  args[0] should be the exec name (i.e. /bin/ls)
+*  @param env enviroment for the new process.  The default is a copy of the current process' enviroment.
+*/
 bool runAttachedProcess(ProcStreams* streams, const char* const args[], const char* const env[] = environ);
 
 /* Fast hash function */
